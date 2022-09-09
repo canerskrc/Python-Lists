f93c847d4054d8d34ad2fa4c9fb959c4de858dca
canersekerci <62672535+canerskrc@users.noreply.github.com>
2022-09-09 20:19:23 +0300

    Python Liste Islemleri

---
+++ main.py
@@ -0,0 +1,88 @@
+
+
+#takimlar=["ga","fb","bjk","ts"]
+#takimlar.append("barcelona")
+#print(takimlar)
+
+
+#sebzeler=["lahana","marul", "pırasa","ıspanak"]
+#sebzeler.insert(2,"patlıcan")
+#print(sebzeler)
+
+
+"""iller1=["kayseri","istanbul","ankara","kocaeli"]
+iller2=["malatya", "izmir", "adana", "konya"]
+iller1= iller2.copy()
+print(iller1)
+
+
+takimlar1=["GS","BJK","FB","BJK","TS"]
+print(takimlar1.count("BJK"))
+
+"""
+"""kus1=["bıldırcın","papağan","serçe","karga","kartal","akbaba"]
+kus2=["baykuş","muhabbet kuşu"]
+kus1.extend(kus2)
+print(kus1)"""
+
+
+"""a=["asd","dffg","dfs"]
+print(a.index("dfs"))
+
+kus1.pop(2)
+print(kus1)
+
+kus1.remove("papağan")
+print(kus1)
+
+
+a.reverse()
+print(a)
+
+kus1.sort()
+print(kus1)
+
+del kus1[4]
+print(kus1)
+
+b="CANERŞEKERCİ"
+print(len(b))"""
+
+
+
+
+"""a= [1,2,3]
+b=[4,5,6]
+c=[7,8,9]
+
+yeniliste=[a,b,c]
+print(yeniliste)
+
+
+sebzeler=[]
+sebzeler.append(['kırmızı','turp'])
+sebzeler.append(['yeşil','ıspanak'])
+sebzeler.append(['turuncu','havuç'])
+print(sebzeler)
+
+
+liste=[1,2,3,4,5,'Ankara']
+print(liste)
+liste[0]=str("kayseri")
+liste[2]=float(1.5)
+liste[4]=int(20)
+print(liste)
+
+
+cumle="23 nisan herkese kutlu olsun"
+kelimeler=cumle.split(" ")
+print("Cümlenizde",len(kelimeler),"adet kelime vardır")"""
+
+
+
+
+
+
+
+
+
